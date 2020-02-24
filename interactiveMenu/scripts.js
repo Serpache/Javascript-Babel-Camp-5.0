88147781629b66337d2f486ef0156122aec5110f
Sergio Pacheco Fernández <sergio.pacheco@babel.es>
2020-02-24 13:42:28 +0100

    all links close menu

--- interactiveMenu/scripts.js
+++ interactiveMenu/scripts.js
@@ -2,17 +2,20 @@ const buttonOpen = document.getElementById('icon-open');
 const buttonClose = document.getElementById('icon-close');
 const headerVar = document.querySelector('header');
 const mainVar = document.querySelector('main');
-const link = document.querySelector('a');
+const link = document.querySelectorAll('nav a');
 
 function openMenu(){
     headerVar.classList.add("nav-open");
 }
 
 function closeMenu(){
+    if(headerVar.classList.contains("nav-open")){
     headerVar.classList.remove("nav-open");
+    }
 }
 
 buttonOpen.addEventListener('click', openMenu);
 buttonClose.addEventListener('click', closeMenu);
-link.addEventListener('click', closeMenu);
 mainVar.addEventListener('click', closeMenu);
+
+link.forEach(linkA => {linkA.addEventListener('click', closeMenu);})
